3034ad09b5b8a7e3a7023109944fbcf3eec2a6e8
Vladislav Medvedev <v.medvedev@cybertradingmachines.com>
2024-05-27 17:57:55 +0300

    Add MudBlazor

--- src/Payments.Web/Program.cs
+++ src/Payments.Web/Program.cs
@@ -1,10 +1,14 @@
+using MudBlazor.Services;
+
 using Payments.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+       .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
@@ -12,16 +16,13 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseHttpsRedirection()
+   .UseStaticFiles()
+   .UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+   .AddInteractiveServerRenderMode();
 
 app.Run();
